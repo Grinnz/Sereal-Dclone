b24004a63887fb9c088b9308de9d8cc8016557e1
Dan Book <grinnz@grinnz.com>
2016-02-05 03:16:05 -0500

    Bump version

--- lib/Sereal/Dclone.pm
+++ lib/Sereal/Dclone.pm
@@ -6,7 +6,7 @@ use Exporter 'import';
 use Sereal::Decoder 'sereal_decode_with_object';
 use Sereal::Encoder 'sereal_encode_with_object';
 
-our $VERSION = '0.001';
+our $VERSION = '0.002';
 
 our @EXPORT_OK = 'dclone';
 
